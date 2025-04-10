c9571ca037f1692298898817471a7123c0a1e2aa
janakiramkusu <160325078+janakiramkusu@users.noreply.github.com>
2025-04-10 22:24:46 +0530

    Update Login.jsx

--- shopping-app/src/pages/Login.jsx
+++ shopping-app/src/pages/Login.jsx
@@ -36,7 +36,7 @@ function Login() {
   return (
     <div className="login-wrapper">
       <div className="login-container">
-        <img src="assets/login-illustration.svg.svg" alt="Login" className="login-image" />
+        <img src="assets/loginillustration.svg" alt="Login" className="login-image" />
         <h2>Welcome Back 👋</h2>
         <form className="login-form" onSubmit={handleSubmit}>
           <input
